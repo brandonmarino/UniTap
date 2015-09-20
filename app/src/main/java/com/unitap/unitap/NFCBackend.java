16de96b1d1617188b16054ddbd57aa05e6eef5e6
brandonmarino <brandon.a.marino@gmail.com>
2015-09-20 03:09:00 -0400

    Initializing the Repo

---
+++ app/src/main/java/com/unitap/unitap/NFCBackend.java
@@ -0,0 +1,15 @@
+package com.unitap.unitap;
+
+import android.app.Activity;
+
+/**
+ * Created by Brandon Marino on 9/20/2015.
+ */
+public class NFCBackend {
+    private NFCMessagingActivity NFCMessagingActivity;
+
+    public NFCBackend(NFCMessagingActivity NFCMessagingActivity){
+        this.NFCMessagingActivity = NFCMessagingActivity;
+    }
+
+}
---
+++ app/src/main/java/com/unitap/unitap/NFCMessagingActivity.java
@@ -0,0 +1,75 @@
+package com.unitap.unitap;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NFCMessagingActivity extends AppCompatActivity {
+    private EditText outgoingMessage;   //this is where the message from the other user will be stored before it's sent
+    private TextView incomingMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfcmessaging);
+        outgoingMessage = (EditText) findViewById(R.id.outgoing);
+        incomingMessage = (TextView) findViewById(R.id.incoming);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_nfcmessaging, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void clickableButton(View v){
+        //user has chosen to send a message to another device
+        String out = outgoingMessage.getText().toString();
+        if (out != null && !out.equals("")){
+            incomingMessage.setText("Your last sent Message: \n\n" + outgoingMessage.getText().toString());
+            //perform the sending function
+        }else{
+            alertmessage("You can't send an empty message!");
+        }
+    }
+
+    /**
+     * This will just make it easier to send an alert to the user when need be
+     * @param message some message to display
+     */
+    public void alertmessage(String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(NFCMessagingActivity.this).create();
+        alertDialog.setTitle("It's Empty");
+        alertDialog.setMessage(message);
+        alertDialog.show();
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "alright",
+
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+    }
+}

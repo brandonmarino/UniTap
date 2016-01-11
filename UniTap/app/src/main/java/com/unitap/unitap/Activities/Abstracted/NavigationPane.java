44f621e580dc4f94ce89fda5be4a880976e7742b
danielabdalla <daniel.abdalla14@gmail.com>
2016-01-11 12:23:22 -0500

    no message

--- UniTap/app/src/main/java/com/unitap/unitap/Activities/Abstracted/NavigationPane.java
+++ UniTap/app/src/main/java/com/unitap/unitap/Activities/Abstracted/NavigationPane.java
@@ -1,8 +1,12 @@
 package com.unitap.unitap.Activities.Abstracted;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.unitap.unitap.Activities.SettingsActivity;
 import com.unitap.unitap.Activities.WalletActivity;
 import com.unitap.unitap.Activities.testingHCEActivity;
 import com.unitap.unitap.Activities.testingNDEFActivity;

889877bf4590bd7cacb397974a27224fd8d851dd
brandon marino <brandonmarino@cmail.carleton.com>
2016-02-09 17:21:46 -0500

    Implementing more of the CRC/Encapsulation functionality.

--- UniTap/app/src/main/java/com/unitap/unitap/NFCBackend/HCE/Packetization/DeEncapsulation.java
+++ UniTap/app/src/main/java/com/unitap/unitap/NFCBackend/HCE/Packetization/DeEncapsulation.java
@@ -16,34 +16,35 @@ public class DeEncapsulation {
     }
 
     public static Integer getType(byte[] message){
-        byte[] typeByte = getSubarray(message,2,3);
+        byte[] typeByte = getSubArray(message, 2, 3);
         return CRC16.combineBytes(typeByte);
     }
 //--
     public static Integer getPacketNumber(byte[] message){
-        byte[] numByte = getSubarray(message,3,4);
+        byte[] numByte = getSubArray(message, 3, 4);
         return 1;
         //return CRC16.combineBytes(typeByte);
     }
 
     public static byte[] getPhoneIdCrc(byte[] message){
-        byte[] pidByte = getSubarray(message,2,3);
+        byte[] pidByte = getSubArray(message, 2, 3);
         //return CRC16.combineBytes(typeByte);
         byte[] output = {0x00};
         return output;
     }
 
     public static Integer getCompanyId(byte[] message){
-        byte[] cidByte = getSubarray(message,2,3);
+        byte[] cidByte = getSubArray(message, 2, 3);
         //return CRC16.combineBytes(typeByte);
         return 1;
     }
 
     public static byte[] getMessage(byte[] message){
-        byte [] cutMessage  = getSubarray(message,9, message.length);
+        byte [] cutMessage  = getSubArray(message, 9, message.length);
         return cutMessage;
     }
-    private static byte[] getSubarray(byte[] message, int start, int end){
+
+    private static byte[] getSubArray(byte[] message, int start, int end){
         byte[] output = new byte[end-start];
         System.arraycopy(message, start, output, 0, end-start);
         return output;

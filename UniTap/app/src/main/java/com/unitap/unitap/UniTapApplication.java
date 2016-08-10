8d906d396afbca63d0cac96ad2056b7ae68fc1de
danielabdalla <daniel.abdalla14@gmail.com>
2016-08-10 12:58:18 -0400

    Successfully migrated app to back4apps platform

--- UniTap/app/src/main/java/com/unitap/unitap/UniTapApplication.java
+++ UniTap/app/src/main/java/com/unitap/unitap/UniTapApplication.java
@@ -14,7 +14,13 @@ public class UniTapApplication extends Application{
         super.onCreate();
 
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "r05g6kTnuuIxBdl6J5g1skAcGEuNqsEBsvCC8GC2", "XKvIT81OpBTnTRrP2Uf42C08NVMEP20ez5U6y6Se");
+        //Parse.initialize(this, "r05g6kTnuuIxBdl6J5g1skAcGEuNqsEBsvCC8GC2", "XKvIT81OpBTnTRrP2Uf42C08NVMEP20ez5U6y6Se");
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("r05g6kTnuuIxBdl6J5g1skAcGEuNqsEBsvCC8GC2")
+                .clientKey("XKvIT81OpBTnTRrP2Uf42C08NVMEP20ez5U6y6Se")
+                .server("https://parseapi.back4app.com")
+        .build()
+        );
         ParseFacebookUtils.initialize(this);
     }
 

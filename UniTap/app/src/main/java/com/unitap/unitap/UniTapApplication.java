24da718b20a0b05dc7c6684ab6f494dc32345065
danielabdalla <daniel.abdalla14@gmail.com>
2016-08-11 19:52:37 -0400

    About to merge to master

--- UniTap/app/src/main/java/com/unitap/unitap/UniTapApplication.java
+++ UniTap/app/src/main/java/com/unitap/unitap/UniTapApplication.java
@@ -14,7 +14,6 @@ public class UniTapApplication extends Application{
         super.onCreate();
 
         Parse.enableLocalDatastore(this);
-        //Parse.initialize(this, "r05g6kTnuuIxBdl6J5g1skAcGEuNqsEBsvCC8GC2", "XKvIT81OpBTnTRrP2Uf42C08NVMEP20ez5U6y6Se");
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("r05g6kTnuuIxBdl6J5g1skAcGEuNqsEBsvCC8GC2")
                 .clientKey("XKvIT81OpBTnTRrP2Uf42C08NVMEP20ez5U6y6Se")
